d801bf5b1cb3ac9d1525b1215a89554a2a84dcea
Copra24 <folefac24.c@gmail.com>
2024-04-20 20:46:23 +0100

    code update

--- cypress/e2e/TestCase/Home_Page.cy.js
+++ cypress/e2e/TestCase/Home_Page.cy.js
@@ -42,6 +42,39 @@ describe('Validating the logo functionality', ()=>{
           registerPage.confirmPassword()
           registerPage.confirmPasswordInputField(data.userConfirmPassword)
           registerPage.registerButton()
+
+
+
+
+
+          homePage.userNameInputField(data.firstName)
+          homePage.passwordInputField(data.userPassword)
+          homePage.registerLink().click()
+          registerPage.signupHeading()
+          registerPage.notificationMessage()
+          registerPage.firstName()
+          registerPage.firstNameInputField(data.firstName)
+          registerPage.lastName()
+          registerPage.lastNameInputField(data.lastName)
+          registerPage.address()
+          registerPage.addressInputField(data.address)
+          registerPage.city()
+          registerPage.cityInputField(data.city)
+          registerPage.state()
+          registerPage.stateInputField(data.state)
+          registerPage.zipCode()
+          registerPage.zipCodeInputField(data.zipCode)
+          registerPage.phoneNumber()
+          registerPage.phoneNumberInputField(data.phoneNumber)
+          registerPage.sSN()
+          registerPage.sSnInputField(data.SSN)
+          registerPage.userName()
+          registerPage.userNameInputField(data.userName)
+          registerPage.password()
+          registerPage.passwordInputField(data.userPassword)
+          registerPage.confirmPassword()
+          registerPage.confirmPasswordInputField(data.userConfirmPassword)
+          registerPage.registerButt
           
 
 
